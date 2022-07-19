dd5915a91a9f9cbe5c524a7713e944938cd85bbb
dexter <dexter@tapdata.io>
2022-07-19 10:53:29 +0800

    chore: add tapdata-common(sample related) into plugin-kit;

---
+++ plugin-kit/tapdata-common/src/main/java/io/tapdata/common/sample/sampler/SpeedSampler.java
@@ -0,0 +1,32 @@
+package io.tapdata.common.sample.sampler;
+
+import io.tapdata.common.sample.Sampler;
+
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * Incremental record value and last upload time.
+ * Calculate speed by using current time minus last upload time and update last upload time when upload.
+ */
+public class SpeedSampler implements Sampler {
+    private Long lastCalculateTime;
+    private LongAdder totalValue = new LongAdder();
+
+    public void add(long value) {
+        totalValue.add(value);
+    }
+
+    @Override
+    public Number value() {
+        Long temp = lastCalculateTime;
+        lastCalculateTime = System.currentTimeMillis();
+        if(temp != null) {
+            long time = System.currentTimeMillis() - temp;
+            if(time > 0) {
+                return  ((double) totalValue.sumThenReset() / time) * 1000;
+            }
+        }
+        return 0;
+    }
+
+}
